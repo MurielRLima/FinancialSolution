e4743469b95b0326385ce58dabf12cec08783b1e
Muriel Lima <muriel.russo@hotmail.com>
2022-04-16 17:18:54 -0300

    settings and startup

--- FinancialDocument.Api/Startup.cs
+++ FinancialDocument.Api/Startup.cs
@@ -34,6 +34,20 @@ namespace FinancialDocument.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    policy =>
+                    {
+                        policy
+                        .AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .SetIsOriginAllowed((host) => true);
+                    });
+            });
+            //services.AddCors();
+
+
             #region connection
             var server = Configuration["mysql_server"] ?? "localhost";
             var port = Configuration["mysql_port"] ?? "3306";
@@ -140,6 +154,7 @@ namespace FinancialDocument.Api
             // Financial document
             services.AddSwaggerExamplesFromAssemblyOf<DocumentAddCommandExample>();
 
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -157,6 +172,13 @@ namespace FinancialDocument.Api
             app.UseRouting();
 
             app.UseAuthorization();
+
+            app.UseCors(builder => builder
+                            .AllowAnyHeader()
+                            .AllowAnyMethod()
+                            .SetIsOriginAllowed((host) => true)
+                            .AllowCredentials()
+                        );
 
             app.UseEndpoints(endpoints =>
             {
